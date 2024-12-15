33a78a28d00ce3c42ac48314e58c719b246e4835
RahaF <156180919+RahafSaqer4@users.noreply.github.com.>
2024-12-15 16:27:51 +0000

    add all files edit the code

--- script.js
+++ script.js
@@ -3,36 +3,56 @@ let y= "paper";
 let z= "scissors";
 let humanScore=0;
 let computerScore=0;
+let HumanChoice="";
+const paper =document.querySelector(".paper");
+const rock =document.querySelector(".rock");
+const scissors =document.querySelector(".scissors");
+const yourChoice = document.querySelector(".yourChoice");
+const computerChoice = document.querySelector(".computerChoice");
+const yourScore= document.querySelector("#yourScore");
+const computerScore1= document.querySelector("#computerScore");
+const middle= document.querySelector(".middle");
+const playGame1=document.querySelector("#button");
+const on=document.getElementsByClassName("on");
+const onPlay=document.getElementsByClassName("onPlay");
+const playAgain=document.getElementById("playAgain");
+const photo=document.getElementsByClassName("photo");
+const result=document.getElementById("result");
+const up=document.getElementById("up")
+const down=document.getElementById("down")
+startGame()
 
 function getComputerChoice(){
     random=Math.floor(Math.random()*3);
     if (random===0){
+        computerChoice.innerHTML='<img src="images/rock.png">';
         return x;
     }
     else if (random===1){
+        computerChoice.innerHTML='<img src="images/paper.png">';
         return y;
     }
     else {
+        computerChoice.innerHTML='<img src="images/scissors.png">';
         return z;
     }
 }
-
-function getHumanChoice(){
-    let HumanChoice=prompt("What is you choice", "");
-    HumanChoice=HumanChoice.toLowerCase();
+function getHumanChoice(HumanChoice){
     if (HumanChoice===x){
+        yourChoice.innerHTML='<img src="images/rock.png">';
         return x;    
     }
     else if(HumanChoice===y){
+        yourChoice.innerHTML='<img src="images/paper.png">';
         return y;
     }
     else if(HumanChoice===z){
+        yourChoice.innerHTML='<img src="images/scissors.png">';
         return z;
     }
 }
-
 function playRound(humanChoice,ComputerChoice){
-    const humanSelection = getHumanChoice();
+    const humanSelection = getHumanChoice(HumanChoice);
     const computerSelection = getComputerChoice();
     if (humanSelection===x){
         if(computerSelection===z){
@@ -68,19 +88,84 @@ function playRound(humanChoice,ComputerChoice){
         else {return "Its Draw your score is: "+ humanScore + ", Computer score is: " + computerScore}
     }
 }
-
-function playGame(){
-    console.log(playRound());
-    console.log(playRound());
-    console.log(playRound());
-    console.log(playRound());
-    console.log(playRound());
-    if (computerScore>humanScore){
-        return "Game Over!, Your Score is "+ humanScore + " Computer Score is " + computerScore
-    }
-    else if (computerScore<humanScore){
-        return "Congrats! You Win, Your Score is "+ humanScore + ", Computer Score is " + computerScore
-    }
-    else { return "Its a Draw"} 
+function playGame(){ 
+    paper.addEventListener("click",function(){
+        HumanChoice="paper";
+        playRound();
+        winOrLose();
+    });
+    rock.addEventListener("click",function(){
+        HumanChoice="rock";
+        playRound();
+        winOrLose();
+    
+    });
+    scissors.addEventListener("click",function(){
+        HumanChoice="scissors";
+        playRound();
+        winOrLose();
+    }); 
+    down.style.display="flex"
+    up.style.display="flex";
 }
-console.log(playGame());
+function endGame(){
+    for (let i=0;i<on.length;i++){
+        on[i].style.display="block"
+    }
+    for (let i=0;i<onPlay.length;i++){
+        onPlay[i].style.display="none"
+    }  
+    down.style.display="none"
+    up.style.display="none"
+    humanScore=0;
+    computerScore=0; 
+}
+function winOrLose(){
+    yourScore.innerHTML=`Your Score Is:${humanScore} `;
+    computerScore1.innerHTML=`Computer Score Is:${computerScore}`;
+    if (humanScore ===5 ){
+        endGame();
+        result.innerHTML="<p>Congrats! You Win</p>";}
+    else if( computerScore===5 ){
+        endGame();
+        result.innerHTML="<p>Game Over! You lose</p>" 
+    }
+}
+function startGame(){
+    for (let i=0;i<onPlay.length;i++){
+            onPlay[i].style.display="none"
+        }  
+    for (let i=0;i<photo.length;i++){
+            photo[i].style.display="none"
+        }
+    down.style.display="none"
+    up.style.display="none";
+}
+playAgain.addEventListener("click",function(){
+    for (let i=0;i<on.length;i++){
+        on[i].style.display="none";
+    }
+    for (let i=0;i<onPlay.length;i++){
+        onPlay[i].style.display="flex";
+    }
+    down.style.display="flex"
+    up.style.display="flex"
+    humanScore=0;
+    computerScore=0;
+    yourScore.innerHTML=`Your Score Is:${humanScore} `;
+    computerScore1.innerHTML=`Computer Score Is:${computerScore}`;
+    yourChoice.innerHTML='';
+    computerChoice.innerHTML='';
+    })
+playGame1.addEventListener("click",function(){
+    playGame()
+    playGame1.style.display="none"
+    for (let i=0;i<on.length;i++){
+        on[i].style.display="none";
+    }
+    for (let i=0;i<onPlay.length;i++){
+        onPlay[i].style.display="flex";
+    }
+    down.style.display="flex"
+    })
+
